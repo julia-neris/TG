23afb3a79df9d078ce03e535a161e3e35fd79f0a
juia09 <julia.neris1708@gmail.com>
2026-03-30 16:29:12 -0300

    feat: increase header logo size h-20 and header height h-24

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,13 +27,13 @@ const Header = ({ onLoadTranscription }: HeaderProps) => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 glass-card border-b">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-24">
           {/* Logo */}
           <Link to="/" className="flex items-center group">
             <img
               src="/sinfonia-logo.png"
               alt="Sinfonia"
-              className="h-12 w-auto transition-transform group-hover:scale-105"
+              className="h-20 w-auto transition-transform group-hover:scale-105"
             />
           </Link>
 
